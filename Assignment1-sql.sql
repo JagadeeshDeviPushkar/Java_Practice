c44d121d987d0574478d0ec51fecbeb26d8e2002
JagadeeshDeviPushkar <101493807+JagadeeshDeviPushkar@users.noreply.github.com>
2022-07-28 20:36:00 +0530

    Add files via upload

---
+++ Assignment1-sql.sql
@@ -0,0 +1,5 @@
+-- create table emp(emp_id int, emp_name varchar(15), emp_dept varchar(15), emp_phoneno varchar(10), emp_address varchar(15));
+
+-- alter table emp add joining_date date;
+
+alter table emp alter column emp_phoneno int;
